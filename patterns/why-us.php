8990591d7010ad0f6de178e57e9908e77878f019
hsntareq <hsntareq@gmail.com>
2024-08-08 21:15:38 +0600

    style add why us section

--- patterns/why-us.php
+++ patterns/why-us.php
@@ -16,7 +16,91 @@
 <div class="wp-block-group"><!-- wp:group {"metadata":{"name":"container"},"layout":{"type":"constrained","contentSize":"1170px"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"color":{"text":"#eff2f6"},"elements":{"link":{"color":{"text":"#eff2f6"}}}}} -->
 <h3 class="wp-block-heading has-text-color has-link-color" style="color:#eff2f6">Transforming Ideas into Impact <mark style="background-color:rgba(0, 0, 0, 0);color:#383a3e" class="has-inline-color">Explore Our Expertise</mark></h3>
-<!-- /wp:heading --></div>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>FOR STARTUPS</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":179,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://focotik.test/wp-content/uploads/2024/08/bg-why-us-item-1.png" alt="" class="wp-image-179"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">MVP Design</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Create a digital product, attract investors and new clients</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Explore Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>FOR STARTUPS</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":179,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://focotik.test/wp-content/uploads/2024/08/bg-why-us-item-1.png" alt="" class="wp-image-179"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">MVP Design</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Create a digital product, attract investors and new clients</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Explore Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>FOR STARTUPS</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":179,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://focotik.test/wp-content/uploads/2024/08/bg-why-us-item-1.png" alt="" class="wp-image-179"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">MVP Design</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Create a digital product, attract investors and new clients</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Explore Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
