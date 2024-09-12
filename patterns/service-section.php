cd1a36ea423c944bf108583b3a064e0ce6e850d4
hsntareq <hsntareq@gmail.com>
2024-09-12 14:39:39 +0600

    Service page  done without service wrap section

--- patterns/service-section.php
+++ patterns/service-section.php
@@ -189,6 +189,132 @@
 <p style="margin-top:32px;padding-bottom:0;font-size:18px;font-style:normal;font-weight:500;text-decoration:underline"><a href="https://focotik.test/works/" data-type="page" data-id="15"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">Get Services -&gt;</mark></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"flex ","style":{"spacing":{"padding":{"bottom":"0px"},"margin":{"top":"0px","bottom":"160px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group flex" style="margin-top:0px;margin-bottom:160px;padding-bottom:0px"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"margin":{"bottom":"32px"}}},"layout":{"type":"constrained","wideSize":"469px"}} -->
+<div class="wp-block-group" style="margin-bottom:32px"><!-- wp:heading {"level":5,"fontSize":"large"} -->
+<h5 class="wp-block-heading has-large-font-size">Web design UI UX</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500"}}} -->
+<p style="font-size:18px;font-style:normal;font-weight:500">Crafting unparalleled digital journeys for top-tier brands, we seamlessly integrate the power of artificial intelligence.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:list {"className":"is-style-list-with-circle","style":{"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"600","lineHeight":"2"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontFamily":"manrope"} -->
+<ul style="color:#383a3e;margin-top:0;margin-bottom:0;font-size:20px;font-style:normal;font-weight:600;line-height:2" class="wp-block-list is-style-list-with-circle has-text-color has-link-color has-manrope-font-family"><!-- wp:list-item {"style":{"spacing":{"margin":{"top":"0","bottom":"0px"}}}} -->
+<li style="margin-top:0;margin-bottom:0px">An employee database that scales</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"spacing":{"margin":{"bottom":"0px"}}}} -->
+<li style="margin-bottom:0px">Efficient employee case management</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Smart HR workflows</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<li style="margin-top:0;margin-bottom:0">Insightful analytics</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500","textDecoration":"underline"},"spacing":{"padding":{"bottom":"0"},"margin":{"top":"32px"}}}} -->
+<p style="margin-top:32px;padding-bottom:0;font-size:18px;font-style:normal;font-weight:500;text-decoration:underline"><a href="https://focotik.test/works/" data-type="page" data-id="15"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">Get Services -&gt;</mark></a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":587,"width":"auto","height":"485px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/servcie-card-3.png" alt="" class="wp-image-587" style="width:auto;height:485px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"flex ","style":{"spacing":{"padding":{"bottom":"0px"},"margin":{"top":"0px","bottom":"160px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group flex" style="margin-top:0px;margin-bottom:160px;padding-bottom:0px"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":588,"width":"auto","height":"485px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/service-card-4.png" alt="" class="wp-image-588" style="width:auto;height:485px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"margin":{"bottom":"32px"}}},"layout":{"type":"constrained","wideSize":"469px"}} -->
+<div class="wp-block-group" style="margin-bottom:32px"><!-- wp:heading {"level":5,"fontSize":"large"} -->
+<h5 class="wp-block-heading has-large-font-size">SaaS design</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500"}}} -->
+<p style="font-size:18px;font-style:normal;font-weight:500">Crafting unparalleled digital journeys for top-tier brands, we seamlessly integrate the power of artificial intelligence.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:list {"className":"is-style-list-with-circle","style":{"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"600","lineHeight":"2"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontFamily":"manrope"} -->
+<ul style="color:#383a3e;margin-top:0;margin-bottom:0;font-size:20px;font-style:normal;font-weight:600;line-height:2" class="wp-block-list is-style-list-with-circle has-text-color has-link-color has-manrope-font-family"><!-- wp:list-item {"style":{"spacing":{"margin":{"top":"0","bottom":"0px"}}}} -->
+<li style="margin-top:0;margin-bottom:0px">An employee database that scales</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"spacing":{"margin":{"bottom":"0px"}}}} -->
+<li style="margin-bottom:0px">Efficient employee case management</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Smart HR workflows</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<li style="margin-top:0;margin-bottom:0">Insightful analytics</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500","textDecoration":"underline"},"spacing":{"padding":{"bottom":"0"},"margin":{"top":"32px"}}}} -->
+<p style="margin-top:32px;padding-bottom:0;font-size:18px;font-style:normal;font-weight:500;text-decoration:underline"><a href="https://focotik.test/works/" data-type="page" data-id="15"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">Get Services -&gt;</mark></a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"flex ","style":{"spacing":{"padding":{"bottom":"0px"},"margin":{"top":"0px","bottom":"160px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group flex" style="margin-top:0px;margin-bottom:160px;padding-bottom:0px"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"margin":{"bottom":"32px"}}},"layout":{"type":"constrained","wideSize":"469px"}} -->
+<div class="wp-block-group" style="margin-bottom:32px"><!-- wp:heading {"level":5,"fontSize":"large"} -->
+<h5 class="wp-block-heading has-large-font-size">Web design UI UX</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500"}}} -->
+<p style="font-size:18px;font-style:normal;font-weight:500">Crafting unparalleled digital journeys for top-tier brands, we seamlessly integrate the power of artificial intelligence.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:list {"className":"is-style-list-with-circle","style":{"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"600","lineHeight":"2"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontFamily":"manrope"} -->
+<ul style="color:#383a3e;margin-top:0;margin-bottom:0;font-size:20px;font-style:normal;font-weight:600;line-height:2" class="wp-block-list is-style-list-with-circle has-text-color has-link-color has-manrope-font-family"><!-- wp:list-item {"style":{"spacing":{"margin":{"top":"0","bottom":"0px"}}}} -->
+<li style="margin-top:0;margin-bottom:0px">An employee database that scales</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"spacing":{"margin":{"bottom":"0px"}}}} -->
+<li style="margin-bottom:0px">Efficient employee case management</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Smart HR workflows</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<li style="margin-top:0;margin-bottom:0">Insightful analytics</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"500","textDecoration":"underline"},"spacing":{"padding":{"bottom":"0"},"margin":{"top":"32px"}}}} -->
+<p style="margin-top:32px;padding-bottom:0;font-size:18px;font-style:normal;font-weight:500;text-decoration:underline"><a href="https://focotik.test/works/" data-type="page" data-id="15"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">Get Services -&gt;</mark></a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":587,"width":"auto","height":"485px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/servcie-card-3.png" alt="" class="wp-image-587" style="width:auto;height:485px"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
