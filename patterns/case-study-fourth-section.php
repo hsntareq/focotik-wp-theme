4ff070bf6523bf461b62508abe94590591a4e1a2
hsntareq <hsntareq@gmail.com>
2024-09-23 17:56:55 +0600

    case study fourth section design initiated

---
+++ patterns/case-study-fourth-section.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Title: Case Study fourth section
+ * Slug: focotik/case-study-fourth-section
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Case Study fourth section"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"container"},"style":{"spacing":{"padding":{"bottom":"108px"}}},"layout":{"type":"constrained","wideSize":"1170px"}} -->
+<div class="wp-block-group" style="padding-bottom:108px"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":1065,"width":"1170px","height":"714px","scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/case-study-img-3.png" alt="" class="wp-image-1065" style="object-fit:cover;width:1170px;height:714px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:spacer {"height":"60px","style":{"layout":[]}} -->
+<div style="height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"lineHeight":"1.4","letterSpacing":"-0.01em"},"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}}},"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-text-color has-link-color has-medium-font-size" style="color:#383a3e;letter-spacing:-0.01em;line-height:1.4">Feature</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"style":{"typography":{"lineHeight":"1.4","letterSpacing":"-0.01em"},"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}}},"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-text-color has-link-color has-medium-font-size" style="color:#383a3e;letter-spacing:-0.01em;line-height:1.4">04</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"24px","style":{"layout":[]}} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"grid","columnCount":"2","minimumColumnWidth":null}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Working</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
