586c24513e25c84dd79f02b86d0e4587721033ad
dmalidevs <dmalidevs@gmail.com>
2024-11-14 11:12:21 +0600

    dummy after_setup_theme hook used

--- functions.php
+++ functions.php
@@ -142,9 +142,14 @@ function wpdocs_codex_case_studies_init() {
 		'menu_position'      => 5,
 		'menu_icon'          => 'dashicons-analytics',
 		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+        'show_in_rest'       => true
 	);
 
 	register_post_type( 'case-studies', $args );
 }
-
 add_action( 'init', 'wpdocs_codex_case_studies_init' );
+function mytheme_add_editor_styles() {
+    // Add support for wide and full-width blocks
+    // add_theme_support( 'align-wide' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_editor_styles' );
