16adabedbfd5418f69861ca448064e4281e57229
hsntareq <hsntareq@gmail.com>
2024-11-18 17:02:59 +0600

    padding removed

--- patterns/works.php
+++ patterns/works.php
@@ -8,15 +8,15 @@
 ?>
 <!-- wp:group {"metadata":{"name":"works"},"className":"works","style":{"color":{"background":"#e7e7e7"},"spacing":{"margin":{"top":"0","bottom":"0"}},"background":{"backgroundImage":{"url":"<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/works-img-right-top.png","id":238,"source":"file","title":"works-img-right-top"},"backgroundSize":"343px","backgroundPosition":"100% 0%","backgroundRepeat":"no-repeat"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group works has-background" style="background-color:#e7e7e7;margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"works content"},"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"container"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"180px","bottom":"180px"}}},"layout":{"type":"constrained","contentSize":"1170px"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:180px;padding-bottom:180px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"container"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0px","bottom":"0px"}}},"layout":{"type":"constrained","contentSize":"1170px"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0px;padding-bottom:0px"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3,"className":"main-heading","style":{"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}}}} -->
 <h3 class="wp-block-heading main-heading has-text-color has-link-color" style="color:#383a3e">Intuitive works boosting <mark style="background-color:rgba(0, 0, 0, 0);color:#eb6945" class="has-inline-color">conversions by 800%</mark></h3>
 <!-- /wp:heading -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Check more projects</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Check more projects -></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group -->
