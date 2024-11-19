ac093e5c96e8520c42e0e2984f1fd52e0a1f98a0
hsntareq <hsntareq@gmail.com>
2024-11-19 17:02:35 +0600

    padding adjusted

--- patterns/product-viability-4.php
+++ patterns/product-viability-4.php
@@ -6,8 +6,8 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"className":"product-viability-4","metadata":{"name":"Product viability 2"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"color":{"background":"#181a1d"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group has-background product-viability-4" style="background-color:#181a1d;margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"container"},"style":{"spacing":{"padding":{"top":"180px","bottom":"180px"}}},"layout":{"type":"constrained","wideSize":"1170px"}} -->
+<!-- wp:group {"metadata":{"name":"Product viability 4"},"className":"product-viability-4","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"color":{"background":"#181a1d"},"background":{"backgroundImage":{"url":"<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/product-viability-bg-black.png","id":208,"source":"file","title":"product-viability-bg-black"},"backgroundSize":"314px","backgroundPosition":"100% 0%","backgroundRepeat":"no-repeat"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group product-viability-4 has-background" style="background-color:#181a1d;margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"container"},"style":{"spacing":{"padding":{"top":"180px","bottom":"180px"}}},"layout":{"type":"constrained","wideSize":"1170px"}} -->
 <div class="wp-block-group" style="padding-top:180px;padding-bottom:180px"><!-- wp:heading {"textAlign":"center","level":3,"className":"main-heading","style":{"color":{"text":"#eff2f6"},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"letterSpacing":"-0.035rem"}}} -->
 <h3 class="wp-block-heading has-text-align-center main-heading has-text-color has-link-color" style="color:#eff2f6;letter-spacing:-0.035rem">We fulfill the needs of <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">startups at all stages</mark></h3>
 <!-- /wp:heading -->
@@ -16,9 +16,9 @@
 <div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"className":"gap32 viability-4-card-three","layout":{"type":"grid","columnCount":"3","minimumColumnWidth":null}} -->
-<div class="wp-block-group gap32 viability-4-card-three"><!-- wp:group {"className":"is-style-default","style":{"color":{"text":"#eff2f6","background":"#272b2f"},"spacing":{"padding":{"top":"48px","bottom":"48px","left":"32px","right":"32px"}},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"500"},"layout":{"columnSpan":1,"rowSpan":1},"dimensions":{"minHeight":"188px"},"border":{"radius":"16px"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group is-style-default has-text-color has-background has-link-color" style="border-radius:16px;color:#eff2f6;background-color:#272b2f;min-height:188px;padding-top:48px;padding-right:32px;padding-bottom:48px;padding-left:32px;font-size:20px;font-style:normal;font-weight:500"><!-- wp:image {"id":724,"width":"24px","height":"24px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<!-- wp:group {"className":"gap32 viability-4-card-three","layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+<div class="wp-block-group gap32 viability-4-card-three"><!-- wp:group {"className":"is-style-default","style":{"color":{"text":"#eff2f6","background":"#272b2f"},"spacing":{"padding":{"top":"40px","bottom":"40px","left":"40px","right":"40px"}},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"500"},"layout":{"columnSpan":1,"rowSpan":1},"dimensions":{"minHeight":"188px"},"border":{"radius":"16px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-default has-text-color has-background has-link-color" style="border-radius:16px;color:#eff2f6;background-color:#272b2f;min-height:188px;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px;font-size:20px;font-style:normal;font-weight:500"><!-- wp:image {"id":724,"width":"24px","height":"24px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/testimonial-2-review-circle.png" alt="" class="wp-image-724" style="object-fit:cover;width:24px;height:24px"/></figure>
 <!-- /wp:image -->
 
@@ -35,8 +35,8 @@
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"className":"is-style-default","style":{"color":{"text":"#eff2f6","background":"#272b2f"},"spacing":{"padding":{"top":"48px","bottom":"48px","left":"32px","right":"32px"}},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"500"},"layout":{"columnSpan":1,"rowSpan":1},"dimensions":{"minHeight":"188px"},"border":{"radius":"16px"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group is-style-default has-text-color has-background has-link-color" style="border-radius:16px;color:#eff2f6;background-color:#272b2f;min-height:188px;padding-top:48px;padding-right:32px;padding-bottom:48px;padding-left:32px;font-size:20px;font-style:normal;font-weight:500"><!-- wp:image {"id":724,"width":"24px","height":"24px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<!-- wp:group {"className":"is-style-default","style":{"color":{"text":"#eff2f6","background":"#272b2f"},"spacing":{"padding":{"top":"40px","bottom":"40px","left":"30px","right":"30px"}},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"500"},"layout":{"columnSpan":1,"rowSpan":1},"dimensions":{"minHeight":"188px"},"border":{"radius":"16px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-default has-text-color has-background has-link-color" style="border-radius:16px;color:#eff2f6;background-color:#272b2f;min-height:188px;padding-top:40px;padding-right:30px;padding-bottom:40px;padding-left:30px;font-size:20px;font-style:normal;font-weight:500"><!-- wp:image {"id":724,"width":"24px","height":"24px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/testimonial-2-review-circle.png" alt="" class="wp-image-724" style="object-fit:cover;width:24px;height:24px"/></figure>
 <!-- /wp:image -->
 
@@ -53,8 +53,8 @@
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"className":"is-style-default","style":{"color":{"text":"#eff2f6","background":"#272b2f"},"spacing":{"padding":{"top":"48px","bottom":"48px","left":"32px","right":"32px"}},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"500"},"layout":{"columnSpan":1,"rowSpan":1},"dimensions":{"minHeight":"188px"},"border":{"radius":"16px"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group is-style-default has-text-color has-background has-link-color" style="border-radius:16px;color:#eff2f6;background-color:#272b2f;min-height:188px;padding-top:48px;padding-right:32px;padding-bottom:48px;padding-left:32px;font-size:20px;font-style:normal;font-weight:500"><!-- wp:image {"id":724,"width":"24px","height":"24px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<!-- wp:group {"className":"is-style-default","style":{"color":{"text":"#eff2f6","background":"#272b2f"},"spacing":{"padding":{"top":"40px","bottom":"40px","left":"40px","right":"40px"}},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"500"},"layout":{"columnSpan":1,"rowSpan":1},"dimensions":{"minHeight":"188px"},"border":{"radius":"16px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-default has-text-color has-background has-link-color" style="border-radius:16px;color:#eff2f6;background-color:#272b2f;min-height:188px;padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px;font-size:20px;font-style:normal;font-weight:500"><!-- wp:image {"id":724,"width":"24px","height":"24px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/testimonial-2-review-circle.png" alt="" class="wp-image-724" style="object-fit:cover;width:24px;height:24px"/></figure>
 <!-- /wp:image -->
 
@@ -75,13 +75,13 @@
 <!-- wp:group {"metadata":{"name":"section-two"},"style":{"color":{"background":"#daff99"},"spacing":{"padding":{"top":"108px","bottom":"108px"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group has-background" style="background-color:#daff99;padding-top:108px;padding-bottom:108px"><!-- wp:group {"layout":{"type":"constrained","wideSize":"767px"}} -->
 <div class="wp-block-group"><!-- wp:group {"className":"viability-green","style":{"spacing":{"padding":{"top":"51px","right":"50px","bottom":"48px","left":"50px"}}},"backgroundColor":"white","layout":{"type":"constrained","wideSize":"767px"}} -->
-<div class="wp-block-group has-white-background-color has-background viability-green" style="padding-top:51px;padding-right:50px;padding-bottom:48px;padding-left:50px"><!-- wp:heading {"className":"viability-heading","level":3,"style":{"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}}}} -->
-<h3 class="wp-block-heading has-text-color has-link-color viability-heading" style="color:#383a3e"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">Professionals</mark><br>integrated&nbsp;into your<br>team</h3>
+<div class="wp-block-group viability-green has-white-background-color has-background" style="padding-top:51px;padding-right:50px;padding-bottom:48px;padding-left:50px"><!-- wp:heading {"level":3,"className":"viability-heading","style":{"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}}}} -->
+<h3 class="wp-block-heading viability-heading has-text-color has-link-color" style="color:#383a3e"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">Professionals</mark><br>integrated&nbsp;into your<br>team</h3>
 <!-- /wp:heading -->
 
 <!-- wp:list {"className":"is-style-list-with-circle","style":{"spacing":{"margin":{"top":"40px"}}}} -->
 <ul style="margin-top:40px" class="wp-block-list is-style-list-with-circle"><!-- wp:list-item {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
-<li style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)"><strong>Business-oriented</strong>&nbsp; team focus</li>
+<li style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">Business-oriented  team focus</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
@@ -89,17 +89,18 @@
 <!-- /wp:list-item -->
 
 <!-- wp:list-item {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
-<li style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">Сlose cooperation where you get comfort and&nbsp;flexibility&nbsp;in the <strong>working process</strong></li>
+<li style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">Сlose cooperation where you get comfort and flexibility in the working process</li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} -->
-<li style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">Our hiring system will provide you with the designer for the&nbsp;<strong>immediate and productive start</strong></li>
+<li style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">Our hiring system will provide you with the designer for the immediate and productive start</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
-<!-- wp:group {"className":"gap32 viability-4-card-two","layout":{"type":"grid","columnCount":"2","minimumColumnWidth":null}} -->
+
+<!-- wp:group {"className":"gap32 viability-4-card-two","layout":{"type":"grid","columnCount":2,"minimumColumnWidth":null}} -->
 <div class="wp-block-group gap32 viability-4-card-two"><!-- wp:group {"className":"is-style-default","style":{"color":{"text":"#eff2f6","background":"#272b2f"},"spacing":{"padding":{"top":"73px","bottom":"57px","left":"40px","right":"40px"}},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"500"},"layout":{"columnSpan":1,"rowSpan":1},"dimensions":{"minHeight":"188px"},"border":{"radius":"16px"}},"layout":{"type":"constrained","justifyContent":"left"}} -->
 <div class="wp-block-group is-style-default has-text-color has-background has-link-color" style="border-radius:16px;color:#eff2f6;background-color:#272b2f;min-height:188px;padding-top:73px;padding-right:40px;padding-bottom:57px;padding-left:40px;font-size:20px;font-style:normal;font-weight:500"><!-- wp:image {"id":724,"width":"24px","height":"24px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/testimonial-2-review-circle.png" alt="" class="wp-image-724" style="object-fit:cover;width:24px;height:24px"/></figure>
@@ -135,8 +136,6 @@
 <p class="has-text-color has-link-color" style="color:#d6d8dc;margin-top:24px;font-size:14px;font-style:normal;font-weight:400">Our team will spread our experience in UI/UX and MVP design creation to make your product visually appealing and highly demanded by users.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
-<!-- /wp:group -->
-</div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
-
