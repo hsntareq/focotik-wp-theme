0607627e851b0dbb9da41207a418002e05cca8ae
hsntareq <hsntareq@gmail.com>
2024-11-23 17:15:52 +0600

    Case study block designed and fresh block deleted

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * @since 1.0
  */
 
-if (! defined('ABSPATH')) {
+if (!defined('ABSPATH')) {
     exit;
 }
 
@@ -18,8 +18,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 
 Focotik\Theme_Main::get_instance();
 
-function pr($data, $die = false)
-{
+function pr($data, $die = false) {
     echo '<pre>';
     print_r($data);
     echo '</pre>';
@@ -28,14 +27,13 @@ function pr($data, $die = false)
     }
 }
 
-function wpdocs_register_multiple_blocks()
-{
+function wpdocs_register_multiple_blocks() {
     $build_dir = __DIR__ . '/build/blocks';
 
     foreach (scandir($build_dir) as $result) {
         $block_location = $build_dir . '/' . $result;
 
-        if (! is_dir($block_location) || '.' === $result || '..' === $result) {
+        if (!is_dir($block_location) || '.' === $result || '..' === $result) {
             continue;
         }
 
@@ -47,16 +45,15 @@ add_action('init', 'wpdocs_register_multiple_blocks');
 
 
 
-function create_pages_if_not_exist()
-{
+function create_pages_if_not_exist() {
     $pages = ['Home', 'About', 'Services', 'Clients', 'Works', 'Contact', 'Product Redesign', 'MVP', 'Team Extention', 'Case Study', 'Blog'];
     foreach ($pages as $slug) {
         $existing_page = get_page_by_path(strtolower($slug));
         if (!$existing_page) {
             wp_insert_post([
-                'post_title'   => $slug,
-                'post_status'  => 'publish',
-                'post_type'    => 'page',
+                'post_title'  => $slug,
+                'post_status' => 'publish',
+                'post_type'   => 'page',
             ]);
         }
     }
@@ -64,8 +61,7 @@ function create_pages_if_not_exist()
 add_action('after_switch_theme', 'create_pages_if_not_exist');
 
 // Function to generate and print navigation HTML based on page titles and URLs
-function generate_navigation_html()
-{
+function generate_navigation_html() {
     // Define the array of page titles
     $pages = array(
         'Works',
@@ -78,8 +74,8 @@ function generate_navigation_html()
     // Fetch URLs for the pages using array_map and inline function
     $page_urls = array_map(function ($title) {
         $page = get_posts(array(
-            'post_type' => 'page',
-            'title'     => $title,
+            'post_type'   => 'page',
+            'title'       => $title,
             'numberposts' => 1
         ));
         return !empty($page) ? get_permalink($page[0]->ID) : null;
@@ -108,52 +104,87 @@ HTML;
     return $html;
 }
 
-function add_reset_filter_link($content)
-{
+function add_reset_filter_link($content) {
     $reset_link = '<li class="cat-item' . (is_home() ? ' current-cat' : '') . '"><a href="' . home_url('/blog') . '">All topics</a></li>';
     return $reset_link . $content;
 }
 add_filter('wp_list_categories', 'add_reset_filter_link');
 
 function wpdocs_codex_case_studies_init() {
-	$labels = array(
-		'name'                  => _x( 'Case Studies', 'Post type general name', 'focotik' ),
-		'singular_name'         => _x( 'Case Study', 'Post type singular name', 'focotik' ),
-		'menu_name'             => _x( 'Case Studies', 'Admin Menu text', 'focotik' ),
-		'name_admin_bar'        => _x( 'New Case Study', 'Add New on Toolbar', 'focotik' ),
-		'add_new'               => _x( 'Add New', 'case study', 'focotik' ),
-		'add_new_item'          => __( 'Add New Case Study', 'focotik' ),
-		'edit_item'             => __( 'Edit Case Study', 'focotik' ),
-		'new_item'              => __( 'New Case Study', 'focotik' ),
-		'view_item'             => __( 'View Case Study', 'focotik' ),
-		'all_items'             => __( 'All Case Studies', 'focotik' ),
-		'search_items'          => __( 'Search Case Studies', 'focotik' ),
-		'not_found'             => __( 'No case studies found.', 'focotik' ),
-		'not_found_in_trash'    => __( 'No case studies found in Trash.', 'focotik' ),
-	);
+    $labels = array(
+        'name'               => _x('Case Studies', 'Post type general name', 'focotik'),
+        'singular_name'      => _x('Case Study', 'Post type singular name', 'focotik'),
+        'menu_name'          => _x('Case Studies', 'Admin Menu text', 'focotik'),
+        'name_admin_bar'     => _x('New Case Study', 'Add New on Toolbar', 'focotik'),
+        'add_new'            => _x('Add New', 'case study', 'focotik'),
+        'add_new_item'       => __('Add New Case Study', 'focotik'),
+        'edit_item'          => __('Edit Case Study', 'focotik'),
+        'new_item'           => __('New Case Study', 'focotik'),
+        'view_item'          => __('View Case Study', 'focotik'),
+        'all_items'          => __('All Case Studies', 'focotik'),
+        'search_items'       => __('Search Case Studies', 'focotik'),
+        'not_found'          => __('No case studies found.', 'focotik'),
+        'not_found_in_trash' => __('No case studies found in Trash.', 'focotik'),
+    );
 
-	$args = array(
-		'labels'             => $labels,
-		'public'             => true,
-		'publicly_queryable' => true,
-		'show_ui'            => true,
-		'show_in_menu'       => true,
-		'query_var'          => true,
-		'rewrite'            => array( 'slug' => 'case-studies' ),
-		'capability_type'    => 'post',
-		'has_archive'        => true,
-		'hierarchical'       => false,
-		'menu_position'      => 5,
-		'menu_icon'          => 'dashicons-analytics',
-		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
-        'show_in_rest'       => true
-	);
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array('slug' => 'case-studies'),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'menu_icon'          => 'dashicons-analytics',
+        'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+        'show_in_rest'       => true,
+        'taxonomies'         => array('category', 'post_tag'),
+    );
 
-	register_post_type( 'case-studies', $args );
+    register_post_type('case-studies', $args);
 }
-add_action( 'init', 'wpdocs_codex_case_studies_init' );
+add_action('init', 'wpdocs_codex_case_studies_init');
 function mytheme_add_editor_styles() {
     // Add support for wide and full-width blocks
     // add_theme_support( 'align-wide' );
 }
-add_action( 'after_setup_theme', 'mytheme_add_editor_styles' );
+add_action('after_setup_theme', 'mytheme_add_editor_styles');
+function render_post_item($post, $signle = false) {
+    $post_thumbnail = get_the_post_thumbnail($post->ID, '', ['style' => 'border-radius:8px;max-height:518px;object-fit:cover;width:100%']);
+    if ($post_thumbnail == '' || !has_post_thumbnail($post->ID)) {
+        $placeholder_image = FOCOTIK_THEME_URI . 'assets/images/placeholder-images/380x150.png';
+        // Fallback image if no thumbnail
+        $post_thumbnail = '<img src="' . $placeholder_image . '" alt="" class="wp-image-945" style="border-radius:8px;width:100%;height:380px" />';
+    }
+    // Get post categories
+    $post_categories = wp_get_post_categories($post->ID);
+
+    // Start building the output
+    $output = '<div class="wp-block-group">
+    <a href="' . get_the_permalink($post->ID) . '">
+        <figure class="wp-block-image size-full is-resized has-custom-border post-thumbnail">' . $post_thumbnail . '</figure>
+    </a>
+    <div class="flex gap8 wrap" style="margin-top:var(--wp--preset--spacing--40); margin-bottom:var(--wp--preset--spacing--40)">';
+
+    // Loop through categories
+    if (!empty($post_categories)) {
+        foreach ($post_categories as $category) {
+            $cat = get_category($category);
+            $output .= '<div class="category-button"><a href="' . get_term_link($cat) . '">' . esc_html($cat->name) . '</a></div>';
+        }
+    }
+
+    $output .= '</div>
+        <h5 class="wp-block-heading">
+            <a href="' . get_the_permalink($post->ID) . '">
+                <span class="underline">' . esc_html($post->post_title) . '</span>
+            </a>
+        </h5>
+    </div>';
+
+    return $output;
+}
--- src/blocks/case-study/render.php
+++ src/blocks/case-study/render.php
@@ -10,29 +10,10 @@ $case_studies = get_posts(array("post_type" => 'case-studies', 'posts_per_page' 
 		<div class="wp-block-group is-nowrap" style="display:flex;flex-wrap:nowrap;justify-content:space-between;align-items:center">
 			<h3 class="main-heading" style="color:#383a3e">Intuitive works boosting <mark style="background-color:rgba(0, 0, 0, 0);color:#eb6945" class="has-inline-color">conversions by 800%</mark></h3>
 			<div class="wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
-				<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Check more projects -&gt;</a></div>
+				<div class="wp-block-button"><a href="<?php echo esc_url(home_url('/works')) ?>" class="wp-block-button__link wp-element-button">Check more projects -&gt;</a></div>
 			</div>
 		</div>
 		<div style="height:48px;margin-top:24px" aria-hidden="true"></div>
-		<?php
-		function render_post_item($post,$signle = false) {
-			if (has_post_thumbnail($post->ID)) {
-				$post_thumbnail = get_the_post_thumbnail($post->ID, '', 'style=border-radius:8px;max-height:518px;object-fit:cover;width:100%');
-			} else {
-				$post_thumbnail = '<img src="https://via.placeholder.com/380x150" height="50" alt="" class="wp-image-945" style="border-radius:8px;width:100%;height:380px" />';
-			}
-
-			return '<div class="wp-block-group">
-			<a href="'.get_the_permalink($post->ID).'">
-					<figure class="wp-block-image size-full is-resized has-custom-border"> ' . $post_thumbnail . '</figure></a>
-					<div class="wp-block-buttons gap8" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-						<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">WEBSITE</a></div>
-						<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">BUSINESS</a></div>
-					</div>
-					<h5 class="wp-block-heading" style="margin-top:32px;font-style:normal;font-weight:600"><a href="'.get_the_permalink($post->ID).'">' . esc_html(__($post->post_title)) . '</a></h5>
-				</div>';
-		}
-		?>
 
 		<?php if ($case_studies): ?>
 			<div class="posts-container">
@@ -51,91 +32,14 @@ $case_studies = get_posts(array("post_type" => 'case-studies', 'posts_per_page' 
 
 						$index++; // Increment index to skip the next post as it’s already displayed
 						echo '</div>'; // Close the two-item row
+						echo '<hr style="margin-bottom:80px;border-color:#C6CBCE">';
 					} elseif ($index % 2 !== 0 || $index === count($case_studies) - 1) {
 						echo '</div>'; // Close the one-item row
+						echo '<hr style="margin-bottom:80px;border-color:#C6CBCE">';
 					}
 					?>
 				<?php endforeach; ?>
 			</div>
 		<?php endif; ?>
-
-
-
-
-
-
-
-		<!--<div class="wp-block-group gap32 grid-column-auto" style="margin-top:0;margin-bottom:0;display: grid;
-  grid-template-columns: repeat(2,minmax(0,1fr));">
-			<?php
-			// pr($case_studies[0]->post_content);
-			foreach ($case_studies as $case_study): ?>
-				<div class="wp-block-group">
-					<figure class="wp-block-image size-full is-resized has-custom-border"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>/assets/images/work-img-1.png" alt="" class="wp-image-945" style="border-radius:8px" /></figure>
-					<div class="wp-block-buttons gap8" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-						<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">WEBSITE</a></div>
-						<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">BUSINESS</a></div>
-					</div>
-					<h5 class="wp-block-heading"><?php echo esc_html(__($case_study->post_title)) ?></h5>
-				</div>
-			<?php endforeach; ?>
-			<!-- <div class="wp-block-group">
-				<figure class="wp-block-image size-full is-resized has-custom-border"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>/assets/images/work-img-2.png" alt="" class="wp-image-946" style="border-radius:8px;width:569px" /></figure>
-
-				<div class="wp-block-buttons gap8" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">WEBSITE</a></div>
-
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">FINTECH</a></div>
-
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">FINTECH</a></div>
-				</div>
-				<h5 class="wp-block-heading">Torii — Building the world's best home buying experience.</h5>
-			</div> 
-		</div>
-
-		<div style="margin-top:80px;height:80px" aria-hidden="true" class="wp-block-spacer hr"></div>
-
-		<div class="wp-block-group" style="margin-top:0;margin-bottom:0;display:grid;grid-template-columns:repeat(1,minmax(0,1fr))">
-			<div class="wp-block-group">
-				<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>/assets/images/work-img-3.png" alt="" class="wp-image-953" style="border-radius:8px;object-fit:cover;" /></figure>
-
-				<div class="wp-block-buttons gap8" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">WEBSITE</a></div>
-
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">BUSINESS</a></div>
-				</div>
-				<h5 class="wp-block-heading" style="margin-top:32px;font-style:normal;font-weight:600">Wonde — Transforming the global EdTech industry with a new brand
-					strategy and website.</h5>
-			</div>
-		</div>
-
-		<div style="margin-top:80px;height:80px" aria-hidden="true" class="wp-block-spacer hr"></div>
-
-		<div class="wp-block-group gap32 grid-column-auto" style="margin-top:0;margin-bottom:0;display: grid;
-  grid-template-columns: repeat(2,minmax(0,1fr));">
-			<div class="wp-block-group">
-				<figure class="wp-block-image size-full is-resized has-custom-border"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>/assets/images/work-img-1.png" alt="" class="wp-image-945" style="border-radius:8px;width:569px" /></figure>
-
-				<div class="wp-block-buttons gap8" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">WEBSITE</a></div>
-
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">BUSINESS</a></div>
-				</div>
-
-				<h5 class="wp-block-heading" style="margin-top:32px;font-style:normal;font-weight:600">Torii — Building the world's best home buying experience.</h5>
-			</div>
-
-			<div class="wp-block-group">
-				<figure class="wp-block-image size-full is-resized has-custom-border"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>/assets/images/work-img-2.png" alt="" class="wp-image-946" style="border-radius:8px;width:569px" /></figure>
-
-				<div class="wp-block-buttons gap8" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40)">
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">WEBSITE</a></div>
-
-					<div class="wp-block-button has-custom-font-size is-style-outline" style="font-size:14px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-border-color wp-element-button" style="border-color:#b2b4b8;padding-top:4px;padding-right:12px;padding-bottom:4px;padding-left:12px">BUSINESS</a></div>
-				</div>
-
-				<h5 class="wp-block-heading" style="margin-top:32px;font-style:normal;font-weight:600">Torii — Building the world's best home buying experience.</h5>
-			</div>
-		</div>-->
 	</div>
 </div>
