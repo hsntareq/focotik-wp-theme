e7bd5b00b880382a5dc3aef315cb865e16a76031
hsntareq <hsntareq@gmail.com>
2024-11-25 16:46:05 +0600

    about hero section separated

---
+++ patterns/hero-about.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Title: hero section
+ * Slug: focotik/hero-about
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Hero About"},"className":"hero-section","style":{"background":{"backgroundImage":{"url":"<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/hero-right.png","id":308,"source":"file","title":"hero-right"},"backgroundSize":"344px","backgroundPosition":"100% 0%","backgroundRepeat":"no-repeat"},"color":{"background":"#e7e7e7"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group hero-section has-background" style="background-color:#e7e7e7"><!-- wp:template-part {"slug":"header-white","theme":"focotik-wp-theme","area":"header"} /-->
+
+<!-- wp:group {"metadata":{"name":"Hero Content"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"Container"},"style":{"padding":{"top":"108px","bottom":"108px"},"spacing":{"padding":{"top":"108px"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"1170px"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:108px"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"hero-text-content","style":{"layout":{"selfStretch":"fixed"},"spacing":{"padding":{"right":"120px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group hero-text-content" style="padding-right:120px"><!-- wp:heading {"className":"hero-heading","style":{"typography":{"fontSize":"72px","lineHeight":"1.2","letterSpacing":"-1.9px"},"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}}}} -->
+<h2 class="wp-block-heading hero-heading has-text-color has-link-color" style="color:#383a3e;font-size:72px;letter-spacing:-1.9px;line-height:1.2">We are a worldwide design agency <mark style="background-color:rgba(0, 0, 0, 0);color:#eb6945" class="has-inline-color">increase conversion...</mark></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"hero-text","style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"typography":{"lineHeight":"1.42","letterSpacing":"-0.23px"}},"fontSize":"large"} -->
+<p class="hero-text has-large-font-size" style="margin-top:0;margin-bottom:0;letter-spacing:-0.23px;line-height:1.42">We are a strategic partner for fast-growing tech companies in need of a scalable website.We are a strategic partner for fast-growing tech companies in need of a scalable website.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-buttons" style="margin-top:0;margin-bottom:0"><!-- wp:button {"style":{"color":{"background":"#eb6945"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-background wp-element-button" href="https://calendly.com/hello-focotik/30min" style="background-color:#eb6945" target="_blank" rel="noreferrer noopener">Schedule a Meeting<img class="wp-image-1309" style="width: 24px;" src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/arrow.png" alt=""></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:group {"className":"success-state","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group success-state"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/clutch.png" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"className":"success-item-list","style":{"spacing":{"blockGap":"48px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group success-item-list"><!-- wp:group {"className":"success-item","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group success-item"><!-- wp:heading {"className":"success-heading"} -->
+<h2 class="wp-block-heading success-heading">300+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"success-text"} -->
+<p class="success-text">Clients</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"success-item","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group success-item"><!-- wp:heading {"className":"success-heading"} -->
+<h2 class="wp-block-heading success-heading">One Billion+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"success-text"} -->
+<p class="success-text">Lives Touched</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"success-item","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group success-item"><!-- wp:heading {"className":"success-heading"} -->
+<h2 class="wp-block-heading success-heading">30+</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"success-text"} -->
+<p class="success-text">Global Awards</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"promo video"},"className":"promo-video","style":{"spacing":{"padding":{"bottom":"6.75rem"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group promo-video" style="padding-bottom:6.75rem"><!-- wp:embed {"url":"https://youtu.be/uOHOgI66C28?si=OkGMaQmaC6mBAnI4","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://youtu.be/uOHOgI66C28?si=OkGMaQmaC6mBAnI4
+</div></figure>
+<!-- /wp:embed --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
