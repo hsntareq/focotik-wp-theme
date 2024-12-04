7d5bed1b8a16146e36d284f907f60ff736a0b757
hsntareq <hsntareq@gmail.com>
2024-12-04 12:47:46 +0600

    redesign hero and bg image changed

--- patterns/home-services-wrapper.php
+++ patterns/home-services-wrapper.php
@@ -8,5 +8,27 @@
  */
 ?>
 <!-- wp:group {"metadata":{"name":"Services Wrapper"},"style":{"spacing":{"padding":{"top":"11.25rem","bottom":"11.25rem"},"margin":{"top":"0","bottom":"0"}},"background":{"backgroundImage":{"url":"\u003c?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/works-img-right-top.png","id":317,"source":"file","title":"works-img-right-top"},"backgroundSize":"346px","backgroundPosition":"100% 0%","backgroundRepeat":"no-repeat"}},"layout":{"type":"constrained","contentSize":"1170px"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:11.25rem;padding-bottom:11.25rem"><!-- wp:case-study/case-study /--></div>
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:11.25rem;padding-bottom:11.25rem">
+    <!-- wp:group {"className":"works","layout":{"type":"default"}} -->
+    <div class="wp-block-group works"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+        <div class="wp-block-group"><!-- wp:heading {"level":3,"className":"main-heading","style":{"color":{"text":"#383a3e"},"elements":{"link":{"color":{"text":"#383a3e"}}},"spacing":{"margin":{"top":"0"}}}} -->
+            <h3 class="wp-block-heading main-heading has-text-color has-link-color" style="color:#383a3e;margin-top:0">Intuitive works boosting <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">conversions by 800%</mark></h3>
+            <!-- /wp:heading -->
+
+            <!-- wp:buttons -->
+            <div class="wp-block-buttons"><!-- wp:button -->
+                <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/contact/">Check more projects <img class="wp-image-503" style="width: 24px;" src="<?php echo esc_url(FOCOTIK_THEME_URI) ?>/assets/images/arrow.png" alt=""></a></div>
+                <!-- /wp:button -->
+            </div>
+            <!-- /wp:buttons -->
+        </div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+
+    <!-- wp:spacer {"height":"24px"} -->
+    <div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+    <!-- /wp:spacer -->
+    <!-- wp:case-study/case-study /-->
+</div>
 <!-- /wp:group -->
