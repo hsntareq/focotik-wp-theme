69a039b89e9b3d8df24356e506fca32b30fda01b
hsntareq <hsntareq@gmail.com>
2024-12-05 16:57:12 +0600

    contact form coming from page content now

--- patterns/contact-popup.php
+++ patterns/contact-popup.php
@@ -9,9 +9,7 @@
 <!-- wp:group {"metadata":{"name":"Contact Section"},"className":"popup-contact-form-parent","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"2rem","bottom":"2rem","left":"0","right":"0"}}},"backgroundColor":"primary","layout":{"type":"default"}} -->
 <div class="wp-block-group popup-contact-form-parent has-primary-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:2rem;padding-right:0;padding-bottom:2rem;padding-left:0"><!-- wp:group {"metadata":{"name":"container"},"className":"popup-contact-form contact-section","style":{"spacing":{"padding":{"top":"2.1rem","bottom":"2.1rem"}}},"layout":{"type":"constrained","wideSize":"1170px"}} -->
 <div class="wp-block-group popup-contact-form contact-section" style="padding-top:2.1rem;padding-bottom:2.1rem"><!-- wp:group {"className":"popup-contact-form-child","style":{"border":{"radius":"8px"},"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"32px","bottom":"32px"}},"color":{"background":"#383a3e","text":"#929498"},"elements":{"link":{"color":{"text":"#929498"}}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group popup-contact-form-child has-text-color has-background has-link-color" style="border-radius:8px;color:#929498;background-color:#383a3e;padding-top:32px;padding-right:var(--wp--preset--spacing--50);padding-bottom:32px;padding-left:var(--wp--preset--spacing--50)"><!-- wp:shortcode -->
-[contact-form-7 id="b0de16d" title="Contact form 1"]
-<!-- /wp:shortcode --></div>
+<div class="wp-block-group popup-contact-form-child has-text-color has-background has-link-color" style="border-radius:8px;color:#929498;background-color:#383a3e;padding-top:32px;padding-right:var(--wp--preset--spacing--50);padding-bottom:32px;padding-left:var(--wp--preset--spacing--50)"><!-- wp:post-content /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
