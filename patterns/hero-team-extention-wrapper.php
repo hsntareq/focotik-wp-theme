4965367678fff4ed3a87467fb2291073f6e84f86
hsntareq <hsntareq@gmail.com>
2024-12-08 09:37:18 +0600

    hero tm extension wrapper bg color and image added

--- patterns/hero-team-extention-wrapper.php
+++ patterns/hero-team-extention-wrapper.php
@@ -7,8 +7,8 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"metadata":{"name":"Hero Contact"},"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/hero-right.png","id":308,"source":"file","title":"hero-right"},"backgroundSize":"344px","backgroundPosition":"100% 0%","backgroundRepeat":"no-repeat"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:template-part {"slug":"header-dark","area":"header"} /-->
+<!-- wp:group {"metadata":{"name":"Team Extension Wrapper"},"style":{"background":{"backgroundImage":{"url":"<?php echo esc_url(FOCOTIK_THEME_URI) ?>assets/images/contact-bg.png","id":566,"source":"file","title":"contact-bg"},"backgroundSize":"344px","backgroundPosition":"100% 0%","backgroundRepeat":"no-repeat"},"color":{"background":"#181a1d"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group has-background" style="background-color:#181a1d"><!-- wp:template-part {"slug":"header-dark","area":"header"} /-->
     <!-- wp:pattern {"slug":"focotik/hero-team-extention"} /-->
 </div>
 <!-- /wp:group -->
