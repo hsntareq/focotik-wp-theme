9e58ef870cbfbeaee1b4b40d6871d2baef35c47b
hsntareq <hsntareq@gmail.com>
2024-12-09 17:04:54 +0600

    area of installation pattern created

---
+++ patterns/area-of-installation.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Area of Installation
+ * Slug: focotik/area-of-installation
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Area of Installation"},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"metadata":{"name":"Container"},"layout":{"type":"constrained","contentSize":"1170px"}} -->
+    <div class="wp-block-group"><!-- wp:group {"style":{"dimensions":{"minHeight":"500px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+        <div id="areaMap" class="wp-block-group" style="min-height:500px"></div>
+        <!-- /wp:group -->
+    </div>
+    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
