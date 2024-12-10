604999542335510bf11b750a42bd259ee22a6cb8
hsntareq <hsntareq@gmail.com>
2024-12-10 16:43:50 +0600

    Services lists pattern made responsive

--- patterns/services-list.php
+++ patterns/services-list.php
@@ -7,10 +7,10 @@
  * Inserter: no
  */
 ?>
-<!-- wp:group {"metadata":{"name":"Services list"},"style":{"spacing":{"padding":{"top":"11.25rem","bottom":"11.25rem"},"margin":{"top":"0","bottom":"0"}},"color":{"background":"#272b2f"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group has-background" style="background-color:#272b2f;margin-top:0;margin-bottom:0;padding-top:11.25rem;padding-bottom:11.25rem"><!-- wp:group {"metadata":{"name":"Container"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"1170px"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"Services Heading"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"822px","justifyContent":"left"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:heading {"level":3,"style":{"color":{"text":"#eff2f6"},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"letterSpacing":"-3.5%"}}} -->
+<!-- wp:group {"metadata":{"name":"Services list"},"className":"services-list","style":{"spacing":{"padding":{"top":"11.25rem","bottom":"11.25rem"},"margin":{"top":"0","bottom":"0"}},"color":{"background":"#272b2f"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group services-list has-background" style="background-color:#272b2f;margin-top:0;margin-bottom:0;padding-top:11.25rem;padding-bottom:11.25rem"><!-- wp:group {"metadata":{"name":"Container"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"1170px"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"Services Heading"},"className":"services-list-heading","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained","contentSize":"822px","justifyContent":"left"}} -->
+<div class="wp-block-group services-list-heading" style="margin-top:0;margin-bottom:0"><!-- wp:heading {"level":3,"style":{"color":{"text":"#eff2f6"},"elements":{"link":{"color":{"text":"#eff2f6"}}},"typography":{"letterSpacing":"-3.5%"}}} -->
 <h3 class="wp-block-heading has-text-color has-link-color" style="color:#eff2f6;letter-spacing:-3.5%">All our creative services included in <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-hover-color">these packages</mark></h3>
 <!-- /wp:heading --></div>
 <!-- /wp:group -->
@@ -19,8 +19,9 @@
 <div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
-<!-- wp:group {"metadata":{"name":"Services Content"},"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"6.75rem"}},"layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+<!-- wp:group {"metadata":{"name":"Services Content"},"className":"services-list-content","layout":{"type":"default"}} -->
+<div class="wp-block-group services-list-content"><!-- wp:group {"style":{"spacing":{"blockGap":"6.75rem"}},"layout":{"type":"grid","columnCount":3,"minimumColumnWidth":null}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"default"}} -->
 <div class="wp-block-group"><!-- wp:list {"className":"is-style-list-with-bullet-gray","style":{"elements":{"link":{"color":{"text":"#eff2f6"}}},"color":{"text":"#eff2f6"}}} -->
 <ul style="color:#eff2f6" class="wp-block-list is-style-list-with-bullet-gray has-text-color has-link-color"><!-- wp:list-item -->
 <li>Website design</li>
@@ -49,9 +50,11 @@
 <!-- wp:list-item -->
 <li>Web app design</li>
 <!-- /wp:list-item --></ul>
-<!-- /wp:list -->
+<!-- /wp:list --></div>
+<!-- /wp:group -->
 
-<!-- wp:list {"className":"is-style-list-with-bullet-gray","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:list {"className":"is-style-list-with-bullet-gray","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
 <ul class="wp-block-list is-style-list-with-bullet-gray has-white-color has-text-color has-link-color"><!-- wp:list-item -->
 <li>MVP product design</li>
 <!-- /wp:list-item -->
@@ -79,9 +82,11 @@
 <!-- wp:list-item -->
 <li>UI Animation</li>
 <!-- /wp:list-item --></ul>
-<!-- /wp:list -->
+<!-- /wp:list --></div>
+<!-- /wp:group -->
 
-<!-- wp:list {"className":"is-style-list-with-bullet-gray","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:list {"className":"is-style-list-with-bullet-gray","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
 <ul class="wp-block-list is-style-list-with-bullet-gray has-white-color has-text-color has-link-color"><!-- wp:list-item -->
 <li>Icon sets</li>
 <!-- /wp:list-item -->
@@ -110,6 +115,7 @@
 <li>Design strategy</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
