49d1a67ecde748c88ae4272aa3504e99746d3f54
Hasan Tareq <hsntareq@gmail.com>
2025-02-13 12:23:40 +0600

    added focotik to gitignore

--- src/blocks/case-study/edit.js
+++ src/blocks/case-study/edit.js
@@ -3,34 +3,34 @@ import { useBlockProps, RichText, MediaUpload, MediaUploadCheck } from '@wordpre
 import { Button } from '@wordpress/components';
 
 export default function TabItemEdit({ attributes, setAttributes }) {
-    const { titleImageUrl, content } = attributes;
+	const { titleImageUrl, content } = attributes;
 
-    return (
-        <div {...useBlockProps()}>
-            <div className="tab-title">
-                <MediaUploadCheck>
-                    <MediaUpload
-                        onSelect={(media) => setAttributes({ titleImageUrl: media.url })}
-                        allowedTypes={['image']}
-                        render={({ open }) => (
-                            <Button onClick={open} className="button">
-                                {titleImageUrl ? (
-                                    <img src={titleImageUrl} alt="Tab Title" />
-                                ) : (
-                                    __('Select Image', 'my-plugin')
-                                )}
-                            </Button>
-                        )}
-                    />
-                </MediaUploadCheck>
-            </div>
-            <div className="tab-content">
-                <RichText
-                    value={content}
-                    onChange={(value) => setAttributes({ content: value })}
-                    placeholder={__('Tab Content', 'my-plugin')}
-                />
-            </div>
-        </div>
-    );
+	return (
+		<div {...useBlockProps()}>
+			<div className="tab-title">
+				<MediaUploadCheck>
+					<MediaUpload
+						onSelect={(media) => setAttributes({ titleImageUrl: media.url })}
+						allowedTypes={['image']}
+						render={({ open }) => (
+							<Button onClick={open} className="button">
+								{titleImageUrl ? (
+									<img src={titleImageUrl} alt="Tab Title" />
+								) : (
+									__('Select Image', 'my-plugin')
+								)}
+							</Button>
+						)}
+					/>
+				</MediaUploadCheck>
+			</div>
+			<div className="tab-content">
+				<RichText
+					value={content}
+					onChange={(value) => setAttributes({ content: value })}
+					placeholder={__('Tab Content', 'my-plugin')}
+				/>
+			</div>
+		</div>
+	);
 }
